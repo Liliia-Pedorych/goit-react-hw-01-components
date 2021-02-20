0328e7f3f1fbd6db76096d72a51a8cdffcafc137
Liliia Pedorych <liliapedorych@ukr.net>
2021-02-20 15:34:24 +0200

    Додає стилі в завданні 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React from "react";
-import Profile from "./Profile/Profile";
-import Statistics from "./Statistics/Statistics";
-import FriendList from "./FriendList/FriendList";
-import TransactionHistory from "./TransactionHistory/TransactionHistory";
-import user from "../data/user.json";
-import statisticalData from "../data/statistical-data.json";
-import friends from "../data/friends.json";
-import transactions from "../data/transactions.json";
+import React from 'react';
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import Container from './Container/Container';
+import user from '../data/user.json';
+import statisticalData from '../data/statistical-data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 const App = () => {
   return (
-    <div>
+    <Container>
       <Profile
         name={user.name}
         tag={user.tag}
@@ -21,7 +22,7 @@ const App = () => {
       <Statistics title="Upload stats" stats={statisticalData} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
-    </div>
+    </Container>
   );
 };
 
