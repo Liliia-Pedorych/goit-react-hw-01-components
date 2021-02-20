d6ecb1918c68037b5facc5b496d5547b39b534ee
Liliia Pedorych <liliapedorych@ukr.net>
2021-02-20 18:09:48 +0200

    Updates

---
+++ precache-manifest.ec17a77da6972763d276883655558cd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "162d7dc4e99b9f97a9d3",
+    "url": "/goit-react-hw-01-components/static/css/main.b8563712.chunk.css"
+  },
+  {
+    "revision": "162d7dc4e99b9f97a9d3",
+    "url": "/goit-react-hw-01-components/static/js/main.35583c4b.chunk.js"
+  },
+  {
+    "revision": "b0f8f2e436fc577230d9",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.681d89f7.js"
+  },
+  {
+    "revision": "7ec639825280d7048485",
+    "url": "/goit-react-hw-01-components/static/css/2.95e7ae2f.chunk.css"
+  },
+  {
+    "revision": "7ec639825280d7048485",
+    "url": "/goit-react-hw-01-components/static/js/2.b0d0a72f.chunk.js"
+  },
+  {
+    "revision": "d36e81368b70827cad5364ef6d434a63",
+    "url": "/goit-react-hw-01-components/index.html"
+  }
+];
